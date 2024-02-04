ed4d763ae5d53ad9bbe64999f3cce95db11dc230
IvanR96 <irios96@hotmail.com>
2024-02-03 21:53:08 -0800

    need to create project div

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
-import _ from 'lodash';
+import _, { forEach } from 'lodash';
 
+class Todo{
+    constructor(title, description, checklist = [], notes = ''){
+        this.title = title;
+        this.description = description;
+        this.checklist = checklist;
+        this.notes = notes;
+        this.completed = false;
+    }
+}
+
+function createTodo(title, description, checklist, notes){
+    return new Todo(title, description, checklist, notes);
+}
+
+function addTodoToHTML(todo) {
+    const todoList = document.getElementById('todoContainer');
+
+    // Create list item element
+    const listItem = document.createElement('li');
+
+    // Create and set content for the list item
+    listItem.innerHTML = `<strong>${todo.title}</strong><br>
+                          <em>- ${todo.description}</em><br>
+                          ${createChecklistHTML(todo.checklist)}<br>
+                          Notes: ${todo.notes}`;
+
+    // Append the list item to the todoList
+    todoList.appendChild(listItem);
+}
+
+function createChecklistHTML(checklist) {
+    return checklist.map(item => `<input type="checkbox">${item}`).join('<br>');
+}
 
 function app(){
 
@@ -14,21 +47,29 @@ function app(){
     const appContainter = document.getElementById('app');
 
     appContainter.appendChild(header);
+
+    // create project div
     
+    // todo container div
+    const todoContainer = document.createElement('div');
+    todoContainer.id = 'todoContainer';
 
-    const projectContainer = document.createElement('div');
-    projectContainer.id = 'projectContainer';
+    appContainter.appendChild(todoContainer);
 
+    const myTodo = createTodo(
+        'Learn JavaScript',
+        'Finish the tutorial on classes',
+        ['Read documentation', 'Practice coding'],
+        'Take notes on key concepts'
+    );
 
-
+    addTodoToHTML(myTodo);
 
 
 
 }
 
-function createTask(name, description, checklist, notes){
-     
-}
+
 
 
 app();
