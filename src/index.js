a60650b38936263884d86179c59f4b3b1cec0c93
IvanR96 <irios96@hotmail.com>
2024-02-21 23:00:55 -0800

    src

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 
+
+// adding linter
 const taskForm = document.getElementById("task-form");
 const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 const openTaskFormBtn = document.getElementById("open-task-form-btn");
